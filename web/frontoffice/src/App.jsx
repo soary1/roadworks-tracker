de58c143e3869784c41449a20483da79d6a4aa49
Noah <noahmichaelrandrianah@gmail.com>
2026-01-19 18:51:44 +0300

    map initialisation dans web et changement .yml a la racine

--- web/frontoffice/src/App.jsx
+++ web/frontoffice/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MapContainer
+      center={[-18.91, 47.52]}
+      zoom={12}
+      style={{ height: '100vh', width: '100%' }}
+      scrollWheelZoom
+    >
+      <TileLayer
+        url="http://localhost:8081/styles/basic-preview/512/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors"
+      />
+    </MapContainer>
   )
 }
 
